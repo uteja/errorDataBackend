3556580dba1f2a2d5868b25a300761f4803ebde9
uday teja <udayteja.k@centurylink.com>
2020-11-05 16:06:33 +0530

    adding changes to postData

--- src/main/java/com/example/springbootswagger2/model/PostData.java
+++ src/main/java/com/example/springbootswagger2/model/PostData.java
@@ -3,6 +3,16 @@ package com.example.springbootswagger2.model;
 public class PostData {
 	
 	private String mailId;
+	
+	private String accountNumber;
+	
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+
+	public void setAccountNumber(String accountNumber) {
+		this.accountNumber = accountNumber;
+	}
 
 	public String getMailId() {
 		return mailId;
